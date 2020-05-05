a2d606ae3476f6dd8b0f0df5cf01a4e1202edee7
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-05-05 11:53:21 -0700

    able to return a usercard from the api

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+  };
+
+  componentDidMount() {
+    console.log("app is mounting");
+    axios
+      .get("https://api.github.com/users/nicholas-myers")
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          users: [res.data],
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    console.log("app is rendering");
+    return (
+      <div className="App">
+        <h1>Git User Cards</h1>
+        <div className="users">
+          {this.state.users.map((user) => (
+            <div key={user.id} className="user">
+              <h2>{user.login}</h2>
+              <img src={user.avatar_url} />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
